bfb3f8d02be646960512fc248bef9ff94c835ad5
Sanjay Lall <sanjaylall@google.com>
2023-09-13 11:53:18 -0700

    simplify timing plot

--- src/timingplot.jl
+++ src/timingplot.jl
@@ -1,34 +1,56 @@
 
+const    red = pk.colormap(1)
+const    green = pk.colormap(2)
+const    blue = pk.colormap(3)
+const    yellow = pk.colormap(4)
+const    darkred = pk.colormap(5)
+const    darkgreen = pk.colormap(6)
+const    darkblue = pk.colormap(7)
+const    darkyellow = pk.colormap(8)
+const    lightred = pk.colormap(9)
+const    lightgreen = pk.colormap(10)
+const    lightblue = pk.colormap(11)
+const    lightyellow = pk.colormap(12)
 
-mutable struct Timing
-    axis
+
+default_offsets(i) = 0
+
+Base.@kwdef mutable struct Timing
     n
     edges
-    tmin
-    tmax
-    bufcols
-    linkcols
-    offsets
-    straight
-    arrowpos
-    noderadius
-    nodefontsize
-    drawonlytimes
-    drawedges
-    drawframes
-    linestyle
-    verticalpath
-    numberframes
-    frameradius
-    framefontsize
-    framefontcolor
-    nodefontcolor
-    straightpath
-    nodeborderlinestyle
-    nodefillcolor
-    nodelabelsincludeid
-    scaletype
+    sample = nothing
+    t = nothing
+    ugn = nothing
+    kw = Dict()
+    tmin = 0
+    tmax = 10
+    bufcols = default_bufcols
+    linkcols = default_linkcols
+    offsets = default_offsets
+    straight = false
+    arrowpos = 0.6
+    arrowsize = 0.05
+    noderadius = 10
+    nodefontsize = 14
+    isvisible = (edgeid, theta) -> true
+    drawframes = true
+    linestyle = (edgeid, theta) -> LineStyle(Color(:black),2)
+    verticallinestyle = LineStyle(Color(:black),2)
+    timelinestyle = LineStyle(Color(0,0,0, 0.5), 1)
+    numberframes = false
+    frameradius = 13
+    framefontsize = 12
+    framefontcolor = Color(:white)
+    nodefontcolor = Color(:black)
+    nodeborderlinestyle = LineStyle(Color(:black),1)
+    nodefillcolor = Color(:white)
+    nodelabelsincludeid = false
+    scaletype = :none
+    drawtime = true
 end
+
+
+    
 
 struct Node
     nodeid
@@ -43,6 +65,20 @@ end
 
 getvar(x::Array, i) = x[i]
 getvar(x,i) = x
+
+function default_bufcols(vs::Timing, i)
+    if vs.n == 2
+        return (blue, yellow)[i]
+    end
+    return (blue, green, red, yellow)[1 + i % 4]
+end
+
+function default_linkcols(vs::Timing, i)
+    if vs.n == 2
+        return (red, green)[i]
+    end
+    return (lightblue, lightgreen, lightred, lightyellow)[1 + i % 4]
+end
 
 ##############################################################################
 # graph functions
@@ -83,7 +119,7 @@ end
 function draw_link_frames(ad::AxisDrawable, vs, linkstate, t)
     for mover in linkstate.movers
         alpha = mover.fraction_traveled
-             
+        
         # from x1,y1 to x2,y2
         x1 = linkstate.src
         x2 = (linkstate.src + linkstate.dst)/2
@@ -91,7 +127,7 @@ function draw_link_frames(ad::AxisDrawable, vs, linkstate, t)
         x = x1 .+ alpha*(x2-x1)
         circle(ad, Point(x,t), vs.frameradius;
                scaletype = vs.scaletype, 
-               fillcolor = vs.linkcols[linkstate.edgeid])
+               fillcolor = vs.linkcols(vs, linkstate.edgeid))
         if vs.numberframes
             text(ad, Point(x,t), vs.framefontsize, vs.framefontcolor,
                  string(Int64(round(mover.senders_theta)));
@@ -103,7 +139,7 @@ end
 
 function draw_buffer_frames(ad::AxisDrawable, vs, linkstate, t)
     for buffered_frame in linkstate.occupants
-
+               
         # y2 is the time it was sent + latency
         # equivalently, the time it was received at the tail of the dst buffer
         y2 = buffered_frame.send_time + linkstate.latency
@@ -121,8 +157,8 @@ function draw_buffer_frames(ad::AxisDrawable, vs, linkstate, t)
         
         circle(ad, Point(x, t),  vs.frameradius;
                scaletype = vs.scaletype,
-               fillcolor = vs.bufcols[linkstate.edgeid])
-         if vs.numberframes
+               fillcolor = vs.bufcols(vs, linkstate.edgeid))
+        if vs.numberframes
             text(ad, Point(x,t), vs.framefontsize, vs.framefontcolor,
                  string(Int64(round(buffered_frame.senders_theta)));
                  scaletype = vs.scaletype,
@@ -136,7 +172,7 @@ struct StraightEdge
     src
     dst
     ugn
-    onlytimes
+    #onlytimes
 end
 
 struct BentEdge
@@ -145,16 +181,19 @@ struct BentEdge
     dst
     ugn
     latency
-    onlytimes
+    #onlytimes
 end
 
 function draw_outgoing_edge(ad::AxisDrawable, vs, extgraph, edge::StraightEdge)
     for node in extgraph.nodes[edge.src]
         p1 = Point(node)
         next_node = node_by_theta(extgraph, edge.dst, node.theta + edge.ugn)
-        if !isnothing(next_node) && (isnothing(edge.onlytimes) || node.theta in edge.onlytimes )
+        #if !isnothing(next_node) && (isnothing(edge.onlytimes) || node.theta in edge.onlytimes )
+        if !isnothing(next_node) && vs.isvisible(edge.edgeid, node.theta) 
             p3 = Point(next_node)
-            pth = getvar(vs.straightpath, edge.edgeid)
+            b = (vs.arrowpos, TriangularArrow(; size = vs.arrowsize))
+            linestyle = vs.linestyle(edge.edgeid, node.theta)
+            pth = Path(; arrows = (b,), linestyle)
             pth.points = [p1, p3]
             draw(ad, pth)
         end
@@ -168,16 +207,17 @@ function draw_outgoing_edge(ad::AxisDrawable, vs, extgraph, edge::BentEdge)
         next_node = node_by_theta(extgraph, edge.dst, node.theta + edge.ugn)
         if !isnothing(next_node)
             p3 = Point(next_node)
-            pth = getvar(vs.straightpath, edge.edgeid)
+            b = (vs.arrowpos, TriangularArrow(; size = vs.arrowsize))
+            linestyle = vs.linestyle(edge.edgeid, node.theta)
+            pth = Path(; arrows = (b,), linestyle)
             pth.points = [p1, p2,  p3]
             draw(ad, pth)
-            #line(ad, [p1, p2, p3]; linestyle=vs.linestyle)
         end
     end
 end
 
 function draw_node(ad::AxisDrawable, vs, node)
-    nodetime = Int(round(node.theta  + vs.offsets[node.nodeid]))
+    nodetime = Int(round(node.theta  + vs.offsets(node.nodeid)))
     sl = string(nodetime)
     if vs.nodelabelsincludeid
         sl = string(node.nodeid) * ", " * string(nodetime)
@@ -195,7 +235,8 @@ end
 function draw_outgoing_vertical_edge(ad::AxisDrawable, vs, extgraph, node)
     nn = next_node(extgraph, node)
     if !isnothing(nn)
-        pth = vs.verticalpath
+        a = (vs.arrowpos, TriangularArrow(; size = 0.05))
+        pth = Path(; arrows = (a,), linestyle = vs.verticallinestyle)
         pth.points = [Point(node), Point(nn)]
         draw(ad, pth)
     end
@@ -208,145 +249,118 @@ end
 
 ##############################################################################
 
-function getpaths(; timinggraph_arrowpos = 0.5,
-                  timinggraph_linestyle = LineStyle(Color(:black),2),
-                  kwargs...)
-    linestyle = timinggraph_linestyle
-    a = (timinggraph_arrowpos, TriangularArrow(; size = 0.05))
-    b = (timinggraph_arrowpos, TriangularArrow(; size = 0.05))
-    verticalpath = Path(; arrows = (a,), linestyle)
-    straightpath = Path(; arrows = (b,), linestyle)
-    return verticalpath, straightpath
-end
+# 
+# function getpaths(; timinggraph_arrowpos = 0.5,
+#                   timinggraph_linestyle = LineStyle(Color(:black),2),
+#                   kwargs...)
+#     linestyle = timinggraph_linestyle
+#     a = (timinggraph_arrowpos, TriangularArrow(; size = 0.05))
+#     b = (timinggraph_arrowpos, TriangularArrow(; size = 0.05))
+#     verticalpath = Path(; arrows = (a,), linestyle)
+#     straightpath = Path(; arrows = (b,), linestyle)
+#     return verticalpath, straightpath
+# end
+# 
+# 
+# 
+# function Timing(n, edges, bidirectional)
+#     # red = pk.colormap(1)
+#     # green = pk.colormap(2)
+#     # blue = pk.colormap(3)
+#     # yellow = pk.colormap(4)
+#     # darkred = pk.colormap(5)
+#     # darkgreen = pk.colormap(6)
+#     # darkblue = pk.colormap(7)
+#     # darkyellow = pk.colormap(8)
+#     # lightred = pk.colormap(9)
+#     # lightgreen = pk.colormap(10)
+#     # lightblue = pk.colormap(11)
+#     # lightyellow = pk.colormap(12)
+#    
+#     # m = length(edges)
+#     # if n == 2
+#     #     bufcols = [blue, yellow]
+#     #     linkcols = [red, green]
+#     # else
+#     #     bcols = [blue, green, red, yellow]
+#     #     lcols = [lightblue, lightgreen, lightred, lightyellow]
+# 
+#     #     function b(i)
+#     #         return bcols[1 + i % 4]
+#     #     end
+#         
+#     #     function l(i)
+#     #         return lcols[1 + i % 4]
+#     #     end
+#         
+#     #     bufcols = [b(e) for e=1:m]
+#     #     linkcols = [l(e) for e=1:m]
+#     # end
+#     
+# #    tmax = 10
+# #    tmin = 0
+# #    offsets = zeros(n)
+# #    noderadius = 10
+# #    nodefontsize = 14
+# #    drawonlytimes = nothing
+#                
+# #    drawedges = collect(1:n-1)
+# #    if bidirectional
+# #        drawedges = collect(1:m)
+# #    end
+#     
+# #    drawframes = true
+# #    linestyle = LineStyle(Color(:black),2)
+# 
+# 
+# 
+# #    numberframes = false
+# #    frameradius = 13
+#     #framefontsize = 12
+#     #framefontcolor = Color(:white)
+#     #nodefontcolor = Color(:black)
+# 
+#    # nodeborderlinestyle = LineStyle(Color(:black),1)
+#     #    nodefillcolor = Color(:white)
+#     #nodelabelsincludeid = false
+#     #scaletype = :none
+#     ds = Timing(nothing,
+#                 n,
+#                 edges,
+#  #               tmin,
+#  #               tmax,
+#  #               bufcols,
+#  #               linkcols,
+#  #               offsets,
+# #                false,
+# #                0.6,
+# #                noderadius,
+# #                nodefontsize,
+# #                drawonlytimes,
+# #                drawedges,
+#                 drawframes,
+#                 linestyle,
+# #                verticalpath,
+#                 numberframes,
+#                 frameradius,
+#                 framefontsize,
+#                 framefontcolor,
+#                 nodefontcolor,
+#                 straightpath,
+#                 nodeborderlinestyle,
+#                 nodefillcolor,
+#                 nodelabelsincludeid,
+#                 scaletype
+#                    )
+#     return ds
+# end
 
-
-function Timing(n, edges, bidirectional)
-    red = pk.colormap(1)
-    green = pk.colormap(2)
-    blue = pk.colormap(3)
-    yellow = pk.colormap(4)
-    darkred = pk.colormap(5)
-    darkgreen = pk.colormap(6)
-    darkblue = pk.colormap(7)
-    darkyellow = pk.colormap(8)
-    lightred = pk.colormap(9)
-    lightgreen = pk.colormap(10)
-    lightblue = pk.colormap(11)
-    lightyellow = pk.colormap(12)
-   
-    m = length(edges)
-    if n == 2
-        bufcols = [blue, yellow]
-        linkcols = [red, green]
-    else
-        bcols = [blue, green, red, yellow]
-        lcols = [lightblue, lightgreen, lightred, lightyellow]
-
-        function b(i)
-            return bcols[1 + i % 4]
-        end
-        
-        function l(i)
-            return lcols[1 + i % 4]
-        end
-        
-        bufcols = [b(e) for e=1:m]
-        linkcols = [l(e) for e=1:m]
-    end
-    
-    tmax = 10
-    tmin = 0
-    offsets = zeros(n)
-    noderadius = 10
-    nodefontsize = 14
-    drawonlytimes = nothing
-  
-    drawedges = collect(1:n-1)
-    if bidirectional
-        drawedges = collect(1:m)
-    end
-    
-    drawframes = true
-    linestyle = LineStyle(Color(:black),2)
-
-    verticalpath, straightpath = getpaths()
-
-    numberframes = false
-    frameradius = 13
-    framefontsize = 12
-    framefontcolor = Color(:white)
-    nodefontcolor = Color(:black)
-
-    nodeborderlinestyle = LineStyle(Color(:black),1)
-    nodefillcolor = Color(:white)
-    nodelabelsincludeid = false
-    scaletype = :none
-    ds = Timing(nothing,
-                n,
-                edges,
-                tmin,
-                tmax,
-                bufcols,
-                linkcols,
-                offsets,
-                false,
-                0.6,
-                noderadius,
-                nodefontsize,
-                drawonlytimes,
-                drawedges,
-                drawframes,
-                linestyle,
-                verticalpath,
-                numberframes,
-                frameradius,
-                framefontsize,
-                framefontcolor,
-                nodefontcolor,
-                straightpath,
-                nodeborderlinestyle,
-                nodefillcolor,
-                nodelabelsincludeid,
-                scaletype
-                   )
-    return ds
-end
 
 nodename(i) = string("ABCDEFGHIJKLMNOPQRSTUVWXYZ"[i])
 
-function Timing(n, edges; bidirectional = false, kwargs...)
 
-    vs = Timing(n, edges, bidirectional)
-    setoptions!(vs, "timinggraph_", kwargs...)
+##############################################################################
 
-    vs.verticalpath, vs.straightpath = getpaths(; kwargs...)
-
-    
-    defaults = Dict(
-        :windowbackgroundcolor => hexcol(0xb0b0b0),
-        :axisbox_ymin => vs.tmin,
-        :axisbox_ymax => vs.tmax,
-        :axisbox_xmin => 0.75,
-        :axisbox_xmax => n + 0.25,
-        :ticks_xticks => vcat(0.75, collect(1:n), n+0.25),
-        :ticks_yticks => [0,1],
-        :ticks_xtickstrings => vcat("",  nodename.(collect(1:n)),""),
-        :ticks_ytickstrings => ["",""]
-    )
-    
-    axis = Axis(; merge(defaults, kwargs)...)
-
-    vs.axis = axis
-    return vs
-end
-
-function drawtiming(vs::Timing, sample, t)
-    ad = AxisDrawable(vs.axis)
-    drawaxis(ad)
-    setclipbox(ad)
-    drawtiming(ad, vs, sample, t)
-    return ad
-end
 
 #
 # We probably need something better than this.
@@ -371,47 +385,73 @@ function drawtimingx(ad::AxisDrawable, vs::Timing, ticks, edgedict)
     return extgraph
 end
 
-function makeedge(ls, straight, onlytimes)
+function makeedge(ls, straight)
     if straight
-        return StraightEdge(ls.edgeid, ls.src, ls.dst, ls.nzero, onlytimes)
+        return StraightEdge(ls.edgeid, ls.src, ls.dst, ls.nzero)
     else
-        return BentEdge(ls.edgeid, ls.src, ls.dst, ls.nzero, ls.latency, onlytimes)
+        return BentEdge(ls.edgeid, ls.src, ls.dst, ls.nzero, ls.latency)
     end
 end
 
-function drawtiming(ad::AxisDrawable, vs::Timing, sample, t)
-    #edgedict = Dict(e => makeedge(sample.linkstates[e], vs.straight) for e in vs.drawedges)
-    edgedict = Dict()
-    for e in vs.drawedges
-        if !isnothing(vs.drawonlytimes)
-            onlytimes = [x.t for x in vs.drawonlytimes if x.edgeid == e]
-        else
-            onlytimes = nothing
-        end
-        edges = makeedge(sample.linkstates[e], vs.straight, onlytimes)
-        edgedict[e] = edges
+function drawtiming(ad::AxisDrawable, vs::Timing)
+
+    makeedge(e) = StraightEdge(e, vs.edges[e].src, vs.edges[e].dst, vs.ugn[e])
+    maketicks() = [(tick, tick) for tick = vs.tmin-10:vs.tmax+10]
+
+    n = vs.n
+    m = length(vs.edges)
+    
+    if isnothing(vs.sample)
+        edgedict = Dict( e => makeedge(e) for e=1:m)
+        ticks = [maketicks() for i=1:n]
+    else
+        edgedict = Dict(e => makeedge(vs.sample.linkstates[e], vs.straight) for e in vs.edges)
+        ticks = sample.ticks
     end
 
-
-    extgraph = drawtimingx(ad, vs, sample.ticks, edgedict)
-
-
+    extgraph = drawtimingx(ad, vs, ticks, edgedict)
 
     # horizontal line indicating current value of t
-    if !vs.straight
-        line(ad, Point(-1, t), Point(vs.n+1, t); linestyle = LineStyle(Color(0,0,0, 0.5), 1))
+    if vs.drawtime && !vs.straight
+        line(ad, Point(-1, t), Point(vs.n+1, t); linestyle = vs.timelinestyle)
     end
 
     # draw frames
     if vs.drawframes && !vs.straight
-        for e in vs.drawedges
+        for e in vs.edges
             draw_link_frames(ad, vs, sample.linkstates[e], t)
             draw_buffer_frames(ad, vs, sample.linkstates[e], t)
         end
     end
 end
 
+##############################################################################
 
+Timing(n, edges; kw...) = Timing(; n, edges, kw, allowed_kws(Timing, kw)...)
+
+PlotKit.Axis(timing::Timing; kw...) = Axis(; axis_defaults(timing)..., timing.kw..., kw...)
+
+axis_defaults(timing) = Dict(
+    :windowbackgroundcolor => hexcol(0xb0b0b0),
+    :axisbox_ymin => timing.tmin,
+    :axisbox_ymax => timing.tmax,
+    :axisbox_xmin => 0.75,
+    :axisbox_xmax => timing.n + 0.25,
+    :ticks_xticks => vcat(0.75, collect(1:timing.n), timing.n + 0.25),
+    :ticks_yticks => [0,1],
+    :ticks_xtickstrings => vcat("",  nodename.(collect(1:timing.n)),""),
+    :ticks_ytickstrings => ["",""]
+)
+
+
+function PlotKit.draw(timing::Timing; kw...)
+    axis = Axis(timing; kw...)
+    ad = AxisDrawable(axis)
+    drawaxis(ad)
+    setclipbox(ad)
+    drawtiming(ad, timing)
+    return ad
+end
 
 
 
